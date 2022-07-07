ee06223fc45b794d183c24fb4fd124d0b90c3ddc
Ruslan <ruslanmakhmatov@gmail.com>
2022-07-07 21:57:03 +0600

    'first

---
+++ easy.js
@@ -0,0 +1,5 @@
+let result = document.getElementById("result");
+// Play Buttons
+let paper = document.querySelector(".paper");
+let rock = document.querySelector(".rock");
+let scissors = document.querySelector("scissors");
