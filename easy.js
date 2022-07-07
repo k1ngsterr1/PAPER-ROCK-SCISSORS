566bc325b36d825dfb3b0f94e91bb7232cbee9e7
Ruslan <ruslanmakhmatov@gmail.com>
2022-07-08 00:32:25 +0600

    'version-3.0'

--- easy.js
+++ easy.js
@@ -8,10 +8,19 @@ let paper = document.querySelector(".paper");
 let rock = document.querySelector(".rock");
 let scissors = document.querySelector("scissors");
 
+paper = "";
+rock = "";
+scissors = "";
+
 // Computer Buttons
-let compPaper = document.querySelector(".c-paper");
+let compPaper = (document.querySelector(".c-paper").disabled = true);
 let compRock = document.querySelector(".c-rock");
 let compScissors = document.querySelector(".c-scissors");
+
+// Disable
+compPaper.disabled = true;
+compRock.disabled = true;
+compScissors = true;
 
 // Choices
 let yChoice = document.querySelector(".your-header");
@@ -28,19 +37,22 @@ let compTurn = "";
 
 function sideSelection() {
   let random_number = Math.floor(Math.random() * (7 + 1));
-  if (random_number > 5) {
+  if (random_number > 5.1) {
     yourTurn = true;
     result.textContent = "Your Turn";
+    choice();
   } else {
     yourTurn = false;
   }
-  if (random_number < 5) {
+  if (random_number < 5.1) {
     compTurn = true;
     result.textContent = "Computer Turn";
+    choice();
   } else {
     compTurn = false;
   }
   console.log(compTurn, yourTurn);
+  console.log(random_number);
 }
 
 function startGame() {
@@ -57,9 +69,16 @@ startBtn.addEventListener("click", () => {
   startGame();
 });
 
-function yourChoice() {
+function choice() {
   if (yourTurn == true) {
     yChoice.classList.remove("your-header");
     yChoice.classList.add("your-header-active");
   }
+
+  if (compTurn == true) {
+    compChoice.classList.remove("computer-header");
+    compChoice.classList.add("computer-header-active");
+  }
 }
+
+function computerChoice() {}
